5c109b1d6d683484c5fffb6867140fa43477baf2
Heloise Taillet <ht2394@columbia.edu>
2019-01-03 23:51:56 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,7 +17,7 @@ def first_challenge
   contacts.each do |person, data|
     data.each do |attribute, value|
       if attribute == :favorite_icecream_flavors
-            :favorite_icecream_flavors.pop("strawberry")
+            :favorite_icecream_flavors.delete("strawberry")
             end
           end
         end
